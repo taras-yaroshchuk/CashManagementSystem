eb769de7e45279de2abee1645fdaffb45265c0db
taras_yaroshchuk <taras.yaroshchuk@gmail.com>
2016-02-15 17:20:12 +0200

    Controllers are created

---
+++ src/main/java/com/bionic/edu/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.bionic.edu.controller;
+
+import com.bionic.edu.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @RequestMapping(value = "/customersList", method = RequestMethod.GET)
+    public String getAllCustomers(Model model) {
+        model.addAttribute("customerList", customerService.findAll());
+        return "customersList";
+    }
+
+    @RequestMapping(value = "/*", method = RequestMethod.GET)
+    public String notFound(Model model) {
+
+        return "notFound";
+    }
+}
---
+++ src/main/java/com/bionic/edu/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.bionic.edu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String showHello() {
+        return "hello";
+    }
+
+    @RequestMapping("/*")
+    public String notFound() {
+        return "notFound";
+    }
+}
